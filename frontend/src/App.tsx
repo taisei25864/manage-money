57a15e6cef63b0685e9f30755c7010e5002d7628
taisei <taisei25864@gmail.com>
2025-07-10 00:56:29 +0900

    add connect front back

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import './App.css'
 
+type Pokemon = {
+  name: string;
+  height: number;
+  weight: number;
+  base_experience: number;
+  types: string[];
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState<Pokemon>();
+  const url = "http://localhost:3000/api/pokemon?name=charizard"
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setPokemon(response.data); // APIのレスポンスをgreetに詰める
+    });
+  }, []); // 第二引数の配列に入れた変数が更新されるたび、第一引数の内容が再実行される。今回は再実行の必要は無いので空
+
+  if (pokemon == null) {
+    // greetが空の場合はぐるぐるローディング画面を流す
+    return (
+      <div className="loading">
+        <h1>error</h1>
+      </div>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="greet">
+      <div className="greet-japanese">
+        <h1>{pokemon.name}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="greet-english">
+        <h1>{pokemon.height}</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
